772ebb1c6f6a75ce4a1de9e254c6d9386794dc58
Joy Heron <joy.heron@innoq.com>
2021-03-10 12:21:34 +0100

    Gradle configuration to publish Jar to maven repository

--- src/test/java/com/heron/patternlibrary/components/bootstrap/models/PaginationTest.java
+++ src/test/java/com/heron/patternlibrary/components/bootstrap/models/PaginationTest.java
@@ -3,7 +3,6 @@ package com.heron.patternlibrary.components.bootstrap.models;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class PaginationTest {
   @Test
